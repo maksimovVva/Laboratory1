802f0bdc219ac9d629f2e850eb6d13c4509e48d2
maksimovVva <32110810+maksimovVva@users.noreply.github.com>
2018-01-12 22:53:00 +0300

    checking in parser

--- parse.py
+++ parse.py
@@ -8,77 +8,89 @@ OPERAND_TO_PRIORITY = {
 }
 
 
-def checkCorrect(line):
-    err = None
-    if len(line) == 0:
-        err = "empty string!"
-        return err
-    i = 0
-    d = 0
-    while i < len(line):
-        if line[i] == "(":
-            d += 1
-        elif line[i] == ")":
-            d -= 1
-            if d < 0:
-                err = "incorrect bracket alignment!"
-                break
-        elif line[i] == '*' or line[i] == '/' or line[i] == '+' or line[i] == '-':
-            if i == 0:
-                err = "incorrect beginning of string - \"" + line[i] + "\""
-                break
-            elif (not line[i - 1].isdigit()) and line[i - 1] != '.' and line[i - 1] != ')':
-                err = "incorrect combination - \"" + line[i - 1] + line[i] + "\""
-                break
-            if i == len(line) - 1:
-                err = "incorrect end of string - \"" + line[i] + "\""
-                break
-            elif (not line[i + 1].isdigit()) and line[i + 1] != '.' and line[i + 1] != '(':
-                err = "incorrect combination - \"" + line[i] + line[i + 1] + "\""
-                break
-        elif (not line[i].isdigit()) and line[i] != ".":
-            err = "incorrect token - \"" + line[i] + "\""
-            break
-        i += 1
-    if d > 0 and err is None:
-        err = "incorrect bracket alignment!"
-    return err
-
-
 def parse(line):
-    line = line.replace(" ", "")
-    err = checkCorrect(line)
-    if err is not None:
-        print("ERROR: ", err)
+    if len(line) == 0:
+        print("ERROR: empty string!")
         return None
     res = ""
     stack = []
     i = 0
+    d = 0
+    lastDigFl = False
+    lastDig = ""
     while i < len(line):
         if line[i].isdigit() or line[i] == ".":
+            if lastDigFl or (i > 0 and line[i-1] == ')'):
+                nextDig = ""
+                while i < len(line) and (line[i].isdigit() or line[i] == "."):
+                    nextDig += line[i]
+                    i += 1
+                if lastDigFl:
+                    print("ERROR: incorrect combination - \"", lastDig, nextDig + "\"")
+                else:
+                    print("ERROR: incorrect combination - \" )", nextDig, "\"")
+                return None
             if line[i] == ".":
                 res += '0'
+            lastDig = ""
             while i < len(line) and (line[i].isdigit() or line[i] == "."):
                 res += line[i]
+                lastDig += line[i]
                 i += 1
             i -= 1
             if line[i] == ".":
                 res += '0'
             res += " "
+            lastDigFl = True
         elif line[i] == "(":
+            if lastDigFl:
+                print("ERROR: incorrect combination - \"", lastDig, "(\"")
+                return None
+            elif i > 0 and line[i-1] == ')':
+                print("ERROR: incorrect combination - \" ) ( \"")
+                return None
+            d += 1
             stack.append(line[i])
+            lastDigFl = False
         elif line[i] == ")":
+            d -= 1
+            if d < 0:
+                print("ERROR: incorrect bracket alignment!")
+                return None
+            if i > 0 and (line[i-1] == '+' or line[i-1] == '-' or line[i-1] == '*' or line[i-1] == '/' or line[i-1] == '('):
+                print("ERROR: incorrect combination - \"", line[i-1], line[i], "\"")
+                return None
             while stack[-1] != "(":
                 res += stack[-1] + " "
                 stack.pop()
             stack.pop()
+            lastDigFl = False
         elif line[i] == '*' or line[i] == '/' or line[i] == '+' or line[i] == '-':
+            if i == 0:
+                print("ERROR: incorrect beginning of string - \"" + line[i] + "\"")
+                return None
+            elif line[i-1] == '*' or line[i-1] == '/' or line[i-1] == '+' or line[i-1] == '-' or line[i-1] == '(':
+                print("ERROR: incorrect combination - \"", line[i - 1], line[i],"\"")
+                return None
+            if i == len(line) - 1:
+                print("ERROR: incorrect end of string - \"" + line[i] + "\"")
+                return None
             priority = OPERAND_TO_PRIORITY[line[i]]
             while len(stack) != 0 and priority <= OPERAND_TO_PRIORITY[stack[-1]]:
                 res += stack[-1] + " "
                 stack.pop()
             stack.append(line[i])
+            lastDigFl = False
+        elif line[i] == " ":
+            line = line[:i] + line [i+1:]
+            i -= 1
+        else:
+            print("ERROR: incorrect token - \"" + line[i] + "\"")
+            return None
         i += 1
+    if d > 0:
+        print("ERROR: incorrect bracket alignment!")
+        return None
     while len(stack) != 0:
         res += stack[-1] + " "
         stack.pop()
